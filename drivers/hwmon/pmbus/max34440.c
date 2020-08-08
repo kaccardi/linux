967a8de6ec78687fa458d71e23f26ec9d29c1c4a
Stephen Kitt <steve@sk2.org>
2020-08-08 23:00:04 +0200

    hwmon (pmbus) use simple i2c probe function
    pmbus_do_probe doesn't use the id information provided in its second
    argument, so this can be removed, which then allows using the
    single-parameter i2c probe function ("probe_new") for probes.
    
    This avoids scanning the identifier tables during probes.
    
    Drivers which didn't use the id are converted as-is; drivers which did
    are modified as follows:
    
    * if the information in i2c_client is sufficient, that's used instead
      (client->name);
    * configured v. probed comparisons are performed by comparing the
      configured name to the detected name, instead of the ids; this
      involves strcmp but is still cheaper than comparing all the device
      names when scanning the tables;
    * anything else is handled by calling i2c_match_id() with the same
      level of error-handling (if any) as before.
    
    Additionally, the mismatch message in the ltc2978 driver is adjusted
    so that it no longer assumes that the driver_data is an index into
    ltc2978_id.
    
    Signed-off-by: Stephen Kitt <steve@sk2.org>
    Acked-by: Wolfram Sang <wsa@kernel.org>
    Link: https://lore.kernel.org/r/20200808210004.30880-1-steve@sk2.org
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/pmbus/max34440.c
+++ drivers/hwmon/pmbus/max34440.c
@@ -41,6 +41,8 @@ struct max34440_data {
 
 #define to_max34440_data(x)  container_of(x, struct max34440_data, info)
 
+static const struct i2c_device_id max34440_id[];
+
 static int max34440_read_word_data(struct i2c_client *client, int page,
 				   int phase, int reg)
 {
@@ -461,8 +463,7 @@ static struct pmbus_driver_info max34440_info[] = {
 	},
 };
 
-static int max34440_probe(struct i2c_client *client,
-			  const struct i2c_device_id *id)
+static int max34440_probe(struct i2c_client *client)
 {
 	struct max34440_data *data;
 	int rv;
@@ -471,8 +472,8 @@ static int max34440_probe(struct i2c_client *client,
 			    GFP_KERNEL);
 	if (!data)
 		return -ENOMEM;
-	data->id = id->driver_data;
-	data->info = max34440_info[id->driver_data];
+	data->id = i2c_match_id(max34440_id, client)->driver_data;
+	data->info = max34440_info[data->id];
 
 	if (data->id == max34451) {
 		rv = max34451_set_supported_funcs(client, data);
@@ -480,7 +481,7 @@ static int max34440_probe(struct i2c_client *client,
 			return rv;
 	}
 
-	return pmbus_do_probe(client, id, &data->info);
+	return pmbus_do_probe(client, &data->info);
 }
 
 static const struct i2c_device_id max34440_id[] = {
@@ -499,7 +500,7 @@ static struct i2c_driver max34440_driver = {
 	.driver = {
 		   .name = "max34440",
 		   },
-	.probe = max34440_probe,
+	.probe_new = max34440_probe,
 	.remove = pmbus_do_remove,
 	.id_table = max34440_id,
 };
