ae4a8d47fac2359aadd225279a90fdf366d091f6
Hugh Dickins <hughd@google.com>
2020-09-14 14:57:00 +1000

    shmem: shmem_writepage() split unlikely i915 THP
    drivers/gpu/drm/i915/gem/i915_gem_shmem.c contains a shmem_writeback()
    which calls shmem_writepage() from a shrinker: that usually works well
    enough; but if /sys/kernel/mm/transparent_hugepage/shmem_enabled has been
    set to "force" (documented as "Force the huge option on for all - very
    useful for testing"), shmem_writepage() is surprised to be called with a
    huge page, and crashes on the VM_BUG_ON_PAGE(PageCompound) (I did not find
    out where the crash happens when CONFIG_DEBUG_VM is off).
    
    LRU page reclaim always splits the shmem huge page first: I'd prefer not
    to demand that of i915, so check and split compound in shmem_writepage().
    
    Link: http://lkml.kernel.org/r/alpine.LSU.2.11.2008301401390.5954@eggly.anvils
    Fixes: 2d6692e642e7 ("drm/i915: Start writeback from the shrinker")
    Signed-off-by: Hugh Dickins <hughd@google.com>
    Reviewed-by: Shakeel Butt <shakeelb@google.com>
    Acked-by: Yang Shi <shy828301@gmail.com>
    Cc: Alex Shi <alex.shi@linux.alibaba.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Qian Cai <cai@lca.pw>
    Cc: <stable@vger.kernel.org>	[5.3+]
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/shmem.c
+++ mm/shmem.c
@@ -1362,7 +1362,15 @@ static int shmem_writepage(struct page *page, struct writeback_control *wbc)
 	swp_entry_t swap;
 	pgoff_t index;
 
-	VM_BUG_ON_PAGE(PageCompound(page), page);
+	/*
+	 * If /sys/kernel/mm/transparent_hugepage/shmem_enabled is "force",
+	 * then drivers/gpu/drm/i915/gem/i915_gem_shmem.c gets huge pages,
+	 * and its shmem_writeback() needs them to be split when swapping.
+	 */
+	if (PageTransCompound(page))
+		if (split_huge_page(page) < 0)
+			goto redirty;
+
 	BUG_ON(!PageLocked(page));
 	mapping = page->mapping;
 	index = page->index;
